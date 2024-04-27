2aa123ac0889bf2181fd3605f8e992312d181bcf
ArabellaSabrina <arabella_90@hotmail.com>
2024-04-27 23:26:19 +0200

    feat(ArrayList für ToDo-Sammlung): mehrere User Inputs in einer Liste sammeln.
    - Eine ArrayList kreieren
    - Mit dem Scanner die User Inputs in der Konsole ermöglichen
    - While-Loop um mehrere Aufgabenpunkte zu sammeln und Abbruchbedingung
    einzuführen ('exit')
    - Ausgabe der Todo-Liste und ihrer Eigenschaften (Länge)

--- simpleToDo/src/simpleToDo/simpleTodo.java
+++ simpleToDo/src/simpleToDo/simpleTodo.java
@@ -1,5 +1,8 @@
 package simpleToDo;
 
+	// Imports aufgrund des verwedeten Codes
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class simpleTodo {
@@ -7,7 +10,9 @@ public class simpleTodo {
 	// Einfache Abfrage eines User Inputs zum kreieren einer neuen Aufgabe
 
 	public static void main(String[] args) {
-		
+		/** 
+		*
+		*
 		Scanner reader = new Scanner(System.in);
 		System.out.println("Gib einen To-Do-Listen Punkt ein: ");
 		
@@ -15,7 +20,42 @@ public class simpleTodo {
 		reader.close();
 		
 		System.out.println("...du hast die Aufgabe " + newTask + " zur To-Do-Liste hinzugefügt.");
-
+		*
+		*
+		*/
+		
+		
+	// Mehrere Aufgaben in einer Liste sammeln und Aufrufen (Konsole) mit ArrayList
+		
+		ArrayList<String> TodoArrayList	= new ArrayList<>();	//Import ArrayList
+		
+		Scanner reader = new Scanner(System.in);	//Um User Input in Konsole erhalten
+		
+		
+		/**
+		 *  While-Schlaufe um mehrere Inputs nacheinander zu ermöglichen 
+		 *  und Abbruchbedingung für Eingabe festzulegen mit if-Statement
+		 */
+		
+		while(true) {
+			System.out.println("Gib einen To-Do-Listen Punkt ein (oder 'exit' um zu beenden): ");	// User Inputs erhalten
+			String newTask = reader.nextLine();	// *1 .next() mit .nextLine() ersetzt
+			
+			if (newTask.equals("exit")) {
+				break;
+			}
+			
+			TodoArrayList.add(newTask);
+			System.out.println("...du hast die Aufgabe " + newTask + " zur To-Do-Liste hinzugefügt.");
+		}
+					
+		System.out.println("Die To-Do Liste enthält " + TodoArrayList.size() + " Aufgaben");	// Wie lange ist die ToDo-Liste
+		System.out.println("Deine To-Do Liste: ");
+		System.out.println(TodoArrayList); 	// *1 Abbruch Bedingung für Eingabe muss angepasst werden, dass nicht das Leerzeichen als Ende des Strings erkannt wird, sondern Zeilenumbruch (Enter).
+		
+		reader.close();		// reader erst zum Schluss(nach While-Schlaufe) schliessen, um Eingabe weiter zu ermöglichen
+		
+		
 	}
 
 }

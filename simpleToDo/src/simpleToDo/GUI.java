7a882a6942119e3593b0d7ddb8755d0ef349546b
ArabellaSabrina <arabella_90@hotmail.com>
2024-04-28 23:25:36 +0200

    feat(Funktionen hinzugefügt): In GUI bearbeitbare ToDo-Liste
     - Eingabe der ToDo's via Dialogfenster (Textfeld)
     - Submit-Button um Punkt hinzuzufügen
     - Anzeige der erfassten ToDo's im Textfeld
     - Bearbeiten der Liste im Textfeld
     - Speichern der Liste in einer ArrayList
     - Abschliessen der ToDo-Liste mit Beenden-Button

--- simpleToDo/src/simpleToDo/GUI.java
+++ simpleToDo/src/simpleToDo/GUI.java
@@ -3,72 +3,127 @@
  * Erster Versuch eine Grafische Benutzeroberfläche zu kreieren.
  * Ziele: 
  * - Eingabe der ToDo's via Dialogfenster (Textfeld)
- * - Submit-Button oder Enter um Punkt hinzuzufügen
- * - Anzeige der erfassten ToDo's
- * - Abschliessen der ToDo-Liste mit Beenden-Button der das Fenster automatisch schliesst
+ * - Submit-Button um Punkt hinzuzufügen
+ * - Anzeige der erfassten ToDo's im Textfeld
+ * - Bearbeiten der Liste im Textfeld
+ * - Speichern der Liste in einer ArrayList 
+ * - Abschliessen der ToDo-Liste mit Beenden-Button (der das Fenster automatisch schliesst)
  * 
  */
 package simpleToDo;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class GUI {
+public class GUI implements ActionListener {	// ActionListener für Button-Action Erfassung
+	
+	// Komponenten
+	private static JFrame frame;
+	private static JLabel inputLabel;
+	private static JLabel showList;
+	private static JLabel describeList;
+	private static JTextField userText;
+	private static JScrollPane scrollPane;
+	private static JButton button;
+	private static JButton saveButton;
+	private static JTextArea textArea;
+	private static JButton closeButton;
+	
 
 	public static void main(String[] args) {
 		// JFrame: Erstellt das Fenster
-		JFrame frame = new JFrame("einfache To-Do Liste");
+		frame = new JFrame("einfache To-Do Liste");
 		// Fenster Grösse definieren
 		frame.setSize(450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
+		
 		// In einem Panel können Text, Felder, Buttons, etc. hinzugefügt werden.
 		JPanel topPanel = new JPanel();
-	// JPanel bottomPanel = new JPanel();
-		// panel zu frame hinzufügen
 		frame.add(topPanel);
-	// frame.add(bottomPanel);
-		
 		topPanel.setLayout(null);
 		
-		
-		JLabel inputLabel = new JLabel("Gib einen To-Do-Listen Punkt ein:");
-				
+		inputLabel = new JLabel("Gib einen To-Do-Listen Punkt ein:");
 		inputLabel.setBounds(10, 20, 200, 25);
 		topPanel.add(inputLabel);
 		
-		JTextField userText = new JTextField(20);
+		userText = new JTextField(20);
 		userText.setBounds(10, 42, 200, 25);
 		topPanel.add(userText);	
 		
-		JButton button = new JButton("Submit");
+		button = new JButton("Submit");
 		button.setBounds(210, 41, 80, 25);
+		button.addActionListener(new GUI());
 		topPanel.add(button);
 		
-		JLabel showList = new JLabel("Deine To-Do-Liste:");
+		showList = new JLabel("Deine To-Do-Liste:");
 		showList.setBounds(10, 80, 150, 25);
 		topPanel.add(showList);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(10, 110, 200, 100);
+		describeList = new JLabel("Hier kannst du die Punkte noch bearbeiten...");
+		describeList.setBounds(10, 100, 300, 15);
+		topPanel.add(describeList);
+		
+		textArea = new JTextArea();
+		textArea.setBounds(10, 120, 200, 100);
 		topPanel.add(textArea);
 		
-		JScrollPane scrollPane = new JScrollPane();
+		scrollPane = new JScrollPane();
 		scrollPane.setViewportView(textArea);
-		scrollPane.setBounds(10, 110, 200, 100);
+		scrollPane.setBounds(10, 120, 200, 100);
 		topPanel.add(scrollPane);
 		
-		JButton saveButton = new JButton("Save");
-		saveButton.setBounds(210, 184, 80, 25);
+		saveButton = new JButton("Save");
+		saveButton.setBounds(210, 194, 80, 25);
+		saveButton.addActionListener(new GUI());
 		topPanel.add(saveButton);
+		
+		closeButton = new JButton("Close");
+		closeButton.setBounds(292, 194, 80, 25);
+		closeButton.addActionListener(new GUI());
+		topPanel.add(closeButton);
 		
 		frame.setVisible(true);
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == button) {
+		String task = userText.getText();
+		
+			if (task.isEmpty()) {
+				JOptionPane.showMessageDialog(null, "Bitte Aufgabe aufschreiben!");
+			} else {
+				textArea.append(task + "\n");
+			}
+		} else if (e.getSource() == saveButton) {
+			String textAreaContent = textArea.getText();
+			
+			ArrayList<String> toDoArrayList	= new ArrayList<>();
+			
+			String[] toDoArrayLines = textAreaContent.split("\\n");
+			
+			for (String line : toDoArrayLines) {
+				toDoArrayList.add(line);
+			}
+				System.out.println("Deine To-Do Liste: ");
+				System.out.println(toDoArrayList);	
+		} else if (e.getSource() == closeButton) {
+			frame.dispose();
+		}
+		
+	}
+	
 
 }
